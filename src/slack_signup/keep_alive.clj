b0d552bdd0cf5d2f96f3c4f6ee4245ae0749b732
Jay Zawrotny <jayzawrotny@gmail.com>
2018-08-08 17:15:38 -0400

    Added keep alive class

---
+++ src/slack_signup/keep_alive.clj
@@ -0,0 +1,14 @@
+(ns slack-signup.keep-alive
+  (:require [clojure.core.async :refer [go-loop <! <!! timeout]]
+            [clj-http.client :as http]))
+
+(defn -main
+  "Keep the slack app alive to prevent sleeping... think of this as
+  manual caffiene."
+  []
+  (<!! (go-loop []
+        (do
+          (println "Pinging slack signup app")
+          (http/get "https://slack.venuebook.com")
+          (<! (timeout 900000))
+          (recur)))))
